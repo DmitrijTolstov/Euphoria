4af1af60ac36f2f89479975174e86a32587b6b6e
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-08-30 11:36:28 +0400

    the beginning of the project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import '@/assets/styles/main.scss'
+import App from '@/App.vue'
+
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app
+.use(pinia)
+.mount('#app')
