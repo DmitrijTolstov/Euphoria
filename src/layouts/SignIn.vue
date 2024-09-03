c90db0b71b17548d01c59947da416a9920fb4df4
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-09-04 00:51:19 +0400

    user authorization page

---
+++ src/layouts/SignIn.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HeaderAuthorization from '@/components/HeaderAuthorization.vue';
+import Authorization from '../components/Authorization.vue';
+
+
+
+</script>
+<template>
+    <HeaderAuthorization />
+    <Authorization :title='"Sign in Page"' />
+</template>
+<style scoped></style>
