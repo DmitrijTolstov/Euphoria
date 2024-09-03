e0910b0b3b2e2fd2dab24847760c49944a87fa0d
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-09-04 00:50:09 +0400

    the component of the form header

---
+++ src/components/HeaderAuthorization.vue
@@ -0,0 +1,78 @@
+<script setup>
+import Button from './button.vue';
+
+
+</script>
+
+<template>
+    <div class="container">
+        <header class="header">
+            <img src="@/assets/images/logo/logo.svg" alt="" class="header-logo">
+            <div class="header-input">
+                <label for="search">Search</label>
+                <input class='header-search' type="text" id="search">
+            </div>
+            <div class="header-right">
+                <select class='header-language' name="" id="">
+                    <option value='Russian (Russian Federation)'>Russian (Russian Federation)</option>
+                    <option value='English (united States)'>English (united States)</option>
+                </select>
+                <div class="header-buttons buttons">
+                    <Button class='buttons_login' :title='"Login"' :color='"white"' :width='140' :bg='"$purple"'
+                        :border='false' />
+                    <Button class='buttons_sign' :title='"Sign Up"' :color='"$purple"' :width='156' :bg='"transparent"'
+                        :border='true' />
+                </div>
+            </div>
+        </header>
+    </div>
+    <div class="line"></div>
+</template>
+
+<style scoped lang='scss'>
+.header {
+    display: flex;
+    justify-content: space-between;
+    padding-block: 34px 31px;
+
+    &-right {
+        display: flex;
+        gap: 60px;
+    }
+
+    &-buttons {
+        display: flex;
+        gap: 25px;
+    }
+
+    &-search {
+        padding-block: 12px;
+        padding-inline: 12px;
+        background-color: $light-gray;
+        max-width: 184px;
+        position: relative;
+        background-image: url('@/assets/images/icons/search.svg');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-position-x: 51px;
+        border-radius: 8px;
+    }
+
+    &-input {
+        position: relative;
+
+        label {
+            position: absolute;
+            top: 50%;
+            left: 80px;
+            z-index: 10;
+            transform: translateY(-50%);
+            cursor: auto;
+        }
+    }
+}
+
+.line {
+    @include line
+}
+</style>
