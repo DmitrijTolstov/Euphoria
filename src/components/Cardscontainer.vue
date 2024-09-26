587a97d6cf39d4fd8106e9d759bebf4aa6b9bbd5
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-09-26 22:52:05 +0400

    card block component

---
+++ src/components/Cardscontainer.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { stock } from '../stores/stock';
+import StockCard from './StockCard.vue';
+
+const store = stock()
+
+</script>
+<template>
+
+	<div class="container">
+		<div class="stockCard">
+			<StockCard v-for='card in store.stockCard' :link='card.link' :text='card.text' :title='card.title'
+				:sale='card.sale' :img='card.img' />
+		</div>
+	</div>
+
+</template>
+<style scoped lang='scss'>
+.stockCard {
+	display: flex;
+	gap: 30px;
+	justify-content: space-between;
+}
+</style>
