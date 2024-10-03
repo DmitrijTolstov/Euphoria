dee8b38d7abc2ee4133dda68088cebc1b137cf79
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-03 23:39:47 +0400

    rename component

---
+++ src/layouts/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Arrival from '../components/LinkProduct.vue';
+import Cardscontainer from '../components/DiscountContainer.vue';
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+import Hero from '../components/Hero.vue';
+import ProductCard from '../components/ProductCard.vue';
+import Stock from '../components/Discount.vue';
+
+
+
+</script>
+<template>
+	<Header />
+	<Hero />
+	<div class="container">
+		<Cardscontainer />
+		<Arrival />
+		<Stock />
+	</div>
+	<Footer />
+</template>
+<style scoped lang='scss'></style>
