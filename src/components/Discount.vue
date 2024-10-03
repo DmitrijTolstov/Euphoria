a59df2f380e4ae68e67149b0469239cc80eeb288
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-03 23:34:48 +0400

    discount card component

---
+++ src/components/Discount.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { productCard } from '../stores/ProductCard';
+import StockCard from './DiscountCard.vue';
+
+import { ref } from 'vue'
+
+const store = productCard()
+
+const stockCard = ref(store.stockCard)
+
+</script>
+<template>
+
+
+	<main class="stock">
+		<div class="stock-content">
+			<div class="candle"></div>
+			<h2 class="stock-content__title">Big Saving Zone</h2>
+		</div>
+		<section class="stock-container">
+			<StockCard v-for='card in stockCard' :img='card.image' :title='card.title' :text='card.text'
+				:sale='card.sale' :limited-stock='card.limitedStock' :discount='card.dayDiscount = true' />
+		</section>
+	</main>
+
+
+</template>
+<style scoped lang='scss'>
+.stock {
+	margin-block-end: 100px;
+
+	&-content {
+		display: flex;
+		align-items: center;
+		margin-block-start: 70px;
+
+		.candle {
+			@include purple-candle
+		}
+
+		&__title {
+			font-size: $sup-title;
+			font-weight: 800;
+		}
+	}
+
+	&-container {
+		display: flex;
+		gap: 20px;
+		flex-wrap: wrap;
+		margin-block-start: 70px;
+	}
+
+
+}
+</style>
