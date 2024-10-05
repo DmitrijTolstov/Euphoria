c453c2c9cb6dd128707ccaf1d36dc50029147377
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-05 23:55:42 +0400

    Feedback card output component

---
+++ src/components/Feedback.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { feedback } from '../stores/feedback';
+import FeedbackCard from './FeedbackCard.vue';
+
+import { ref } from 'vue'
+
+const store = feedback()
+
+
+const commentscard = ref(store.comments)
+
+</script>
+<template>
+	<section class="feedback">
+		<div class="feedback-content">
+			<div class="candle"></div>
+			<h2 class="feedback-content__title">
+				Feedback
+			</h2>
+		</div>
+		<div class="feedback-cards">
+			<FeedbackCard v-for='card in commentscard' :key='card' :comment='card.comment' :name='card.name'
+				:images='card.images' :star='card.star' />
+		</div>
+	</section>
+
+</template>
+<style scoped lang='scss'>
+.feedback {
+	margin-block-end: 100px;
+
+	&-content {
+		display: flex;
+		align-items: center;
+		margin-block-end: 70px;
+
+		&__title {
+			font-size: $title;
+			font-weight: 600;
+		}
+
+		.candle {
+			@include purple-candle
+		}
+	}
+
+	&-cards {
+		display: flex;
+		gap: 25px;
+		overflow: hidden;
+	}
+
+
+}
+</style>
--- src/components/FeedbackCard.vue
+++ src/components/FeedbackCard.vue
@@ -3,34 +3,38 @@
 
 import { ref } from 'vue'
 
-const comment = ref('naognsOgvnsogno;sd')
-const name = ref('Floyd Miles')
+const props = defineProps({
+	comment: String,
+	name: String,
+	images: String,
+	star: Number
+})
+
 
 </script>
 <template>
 	<div class="card">
 		<div class="card-content">
-			<img src="@/assets/images/feedback/miles.png" alt="">
+			<img :src="props.images" alt="">
 			<div class="card-content_rating">
-				<svg v-for="(item) in 5" width="20" height="19" viewBox="0 0 20 19" fill="#EDD146"
-					xmlns="http://www.w3.org/2000/svg">
+				<svg v-for="item in props.star" width="20" height="19" viewBox="0 0 20 19" fill="#EDD146">
 					<path
 						d="M19.7476 7.12325L12.7991 6.52407L10.0835 0.126465L7.36789 6.53374L0.419434 7.12325L5.69601 11.6943L4.11111 18.4882L10.0835 14.8835L16.0559 18.4882L14.4806 11.6943L19.7476 7.12325ZM10.0835 13.0763L6.44981 15.2701L7.41621 11.1338L4.20775 8.35058L8.44061 7.98335L10.0835 4.08873L11.7361 7.99301L15.9689 8.36025L12.7604 11.1435L13.7268 15.2797L10.0835 13.0763Z"
 						fill="#EDD146" />
 				</svg>
 			</div>
-			<h5 class="card-content_name">{{ name }}</h5>
-			<p class="card-content_comment">{{ comment }}</p>
+			<h5 class="card-content_name">{{ props.name }}</h5>
+			<p class="card-content_comment">{{ props.comment }}</p>
 		</div>
 	</div>
 </template>
 <style scoped lang='scss'>
 .card {
-	border: 1px solid $gray;
+	border: 1px solid $border-feedback;
 	border-radius: 10px;
 	padding-inline: 23px;
 	padding-block: 23px;
-	max-width: 396px;
+	min-width: 396px;
 	transition: box-shadow .2s linear;
 
 	&:hover {
