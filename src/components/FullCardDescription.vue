48b2ca7698a189a7f02f1813860bc68ee86db506
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-24 22:31:04 +0400

    the general component of the product card

---
+++ src/components/FullCardDescription.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CardContent from './CardComponents/CardContent.vue';
+import CardDescription from './CardComponents/CardDescription.vue';
+
+
+
+</script>
+<template>
+	<CardContent />
+	<CardDescription />
+</template>
+<style scoped lang='scss'></style>
