a8d12c8ca67c7fdca87dafa0d06e1bdeca3e8ee6
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-24 22:32:45 +0400

    A component of the women's section

---
+++ src/layouts/WomensSection.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Footer from '../components/Footer.vue';
+import FullCardDescription from '../components/FullCardDescription.vue';
+import Header from '../components/Header.vue';
+
+
+
+</script>
+<template>
+	<Header />
+	<div class="container">
+		<FullCardDescription />
+	</div>
+	<Footer />
+
+</template>
+<style scoped lang='scss'></style>
