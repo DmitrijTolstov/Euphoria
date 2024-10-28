70ea6f48be83e94786c1a1d0565b35bae1c15c9d
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-28 21:57:32 +0400

    the component for adding a new user address

---
+++ src/components/Account/AddAddress.vue
@@ -0,0 +1,18 @@
+<script setup>
+import FormAccount from '../FormAccount.vue';
+
+
+</script>
+<template>
+
+	<section class="addAddress">
+		<FormAccount />
+	</section>
+
+
+</template>
+<style scoped lang='scss'>
+.addAddress {
+	margin-block-end: 100px;
+}
+</style>
