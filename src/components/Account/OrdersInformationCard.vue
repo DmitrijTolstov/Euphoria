16876b74ece3b5c003a717ff99b00558282e55e2
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-28 21:58:52 +0400

    a component of the user's order card

---
+++ src/components/Account/OrdersInformationCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+</script>
+<template>
+	<div class="orders-info">
+		<div class="orders-information">
+			<h4 class="orders-information__num">Order no: #123456789</h4>
+			<p class="orders-information__text"><b>Order Date </b><span>: 2 June 2023 2:40 PM</span> </p>
+			<p class="orders-information__text"><b>Estimated Delivery Date </b><span>: 8 June 2023</span> </p>
+		</div>
+		<div class="orders-info__status">
+			<p><b>Order Status </b><span>: Inprogress</span> </p>
+			<p><b>Payment Method </b><span>: Cash on delivery </span></p>
+		</div>
+	</div>
+</template>
+<style scoped lang='scss'>
+.orders-info {
+	display: flex;
+	justify-content: space-between;
+	align-items: end;
+	font-size: 14px;
+	background-color: $light-gray;
+	padding-block: 28px;
+	padding-inline: 47px;
+	border-radius: 12px;
+
+	&>div>p>span {
+		opacity: .6;
+	}
+
+	.orders-information {
+		&__num {
+			font-size: 20px;
+			font-weight: 600;
+			margin-block-end: 14px;
+		}
+
+	}
+}
+</style>
