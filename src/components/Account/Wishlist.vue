06f34d2809efbc583816c092c7188c702b52cfce
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-28 22:00:55 +0400

    the user's desire component

---
+++ src/components/Account/Wishlist.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Wishlistcard from './Wishlistcard.vue';
+
+
+</script>
+
+<template>
+	<section class="wishList">
+		<h2 class="wishList__title">Wishlist</h2>
+		<div class="wishList-product">
+			<Wishlistcard />
+		</div>
+	</section>
+</template>
+
+<style scoped lang='scss'>
+.wishList {
+
+	&__title {
+		font-size: $productCardTitle;
+		font-weight: 600;
+	}
+
+	&-product {
+		display: flex;
+		gap: 40px;
+		align-items: center;
+		margin-block: 50px 100px;
+	}
+}
+</style>
