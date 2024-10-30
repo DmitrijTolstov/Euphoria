547257692d7e7d8b4d7a04fcd0c1cb4c5ba07923
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-31 01:00:58 +0400

    the component of the detailed information of the order

---
+++ src/components/Account/OrdersDetails.vue
@@ -0,0 +1,76 @@
+<script setup>
+import Progress from '../Progress.vue';
+import OrdersInformationCard from './OrdersInformationCard.vue';
+import Wishlistcard from './Wishlistcard.vue';
+
+
+</script>
+<template>
+	<section class="ordersDetails">
+		<div class="ordersDetails-arrow">
+		</div>
+		<h2 class="ordersDetails__title">Order Details</h2>
+		<OrdersInformationCard />
+		<Progress />
+		<div class="ordersDetails-info">
+			<p><span>8 June 2023 3:40 PM </span></p>
+			<p>Your order has been successfully verified.</p>
+		</div>
+		<div class="ordersDetails-cards">
+			<Wishlistcard />
+			<Wishlistcard />
+		</div>
+	</section>
+</template>
+<style scoped lang='scss'>
+.ordersDetails {
+	&-arrow {}
+
+	&__title {}
+
+	&-info {
+		background-color: $light-gray;
+		padding-block: 24px;
+		padding-inline: 29px;
+		border-radius: 8px;
+		position: relative;
+		margin-block: 50px 79px;
+
+		p {
+			display: inline;
+			font-weight: 600;
+		}
+
+		&>p {
+			margin-inline-end: 37px;
+
+			span {
+				color: $cardText;
+			}
+		}
+
+		&:before {
+			content: '';
+			position: absolute;
+			width: 42px;
+			height: 42px;
+			background-color: $light-gray;
+			transform: rotate(45deg);
+			top: -10px;
+			left: 120px;
+			z-index: -1;
+		}
+	}
+
+	&-cards {
+		display: flex;
+		flex-direction: column;
+		padding-inline: 50px;
+		background-color: $light-gray;
+		border-radius: 8px;
+		margin-block-end: 100px;
+		overflow: hidden;
+	}
+
+}
+</style>
