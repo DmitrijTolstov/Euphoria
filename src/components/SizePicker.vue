9d23644ed2ad80217a5ee85515a70c9f49ae803f
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-31 00:58:12 +0400

    the size selection component

---
+++ src/components/SizePicker.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+let sizeArr = ['XXS', 'XL', 'XS', 'S', 'M', 'L', 'XXL', '3XL', '4XL']
+
+
+</script>
+<template>
+	<div class="sizePicker" v-for='item in sizeArr'>
+
+		<div class="sizePicker-size">{{ item }} </div>
+
+	</div>
+</template>
+<style scoped lang='scss'>
+.sizePicker {
+	text-align: center;
+
+	&-size {
+		width: 30px;
+		height: 30px;
+		border: 1px solid $border-feedback;
+		border-radius: 8px;
+		font-size: 14px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding-block: 9px;
+		padding-inline: 9px;
+		transition: all .3s linear;
+		cursor: pointer;
+
+		&:hover {
+			background-color: $gray;
+			color: white;
+		}
+	}
+
+}
+</style>
