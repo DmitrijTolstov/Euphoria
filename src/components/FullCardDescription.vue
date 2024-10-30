82998898e7f61cae2b65e3dfacedffff28bf07f2
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-10-31 01:07:39 +0400

    adding a block with similar products

--- src/components/FullCardDescription.vue
+++ src/components/FullCardDescription.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CardContent from './CardComponents/CardContent.vue';
 import CardDescription from './CardComponents/CardDescription.vue';
+import ProductCard from './ProductCard.vue';
 
 
 
@@ -8,5 +9,36 @@ import CardDescription from './CardComponents/CardDescription.vue';
 <template>
 	<CardContent />
 	<CardDescription />
+	<div class="similarProducts">
+		<div class="candle">
+			<div class=""></div>
+			<h1 class='similarProducts__title'>Recently Viewed</h1>
+		</div>
+		<div class="similarProducts-container"></div>
+	</div>
 </template>
-<style scoped lang='scss'></style>
+<style scoped lang='scss'>
+.similarProducts {
+
+	&__title {
+		font-size: $title-footer;
+		font-weight: 600;
+	}
+
+	&-container {
+		display: flex;
+		gap: 37px;
+		flex-wrap: wrap;
+		margin-block: 30px 100px;
+	}
+}
+
+.candle {
+	display: flex;
+	gap: 12px;
+
+	div {
+		@include purple-candle
+	}
+}
+</style>
