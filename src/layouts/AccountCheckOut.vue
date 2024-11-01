229d131a2e2f863729e3099ba08452176519e0c5
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-11-01 23:50:44 +0400

    component of the payment details verification page

---
+++ src/layouts/AccountCheckOut.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Checkout from '../components/Account/Checkout.vue';
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+
+
+
+
+</script>
+<template>
+	<Header />
+	<section class="checkOut">
+		<div class="container">
+			<Checkout />
+		</div>
+	</section>
+	<Footer />
+</template>
+<style scoped lang='scss'></style>
