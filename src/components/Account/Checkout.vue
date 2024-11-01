9150f16f9986088ca6b1b99371dd93dc74e701d7
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-11-01 23:52:28 +0400

    the general component of payment data verification

---
+++ src/components/Account/Checkout.vue
@@ -0,0 +1,37 @@
+<script setup>
+import FormAccount from '@/components//FormAccount.vue';
+import Payment from './Payment.vue';
+import Button from '../button.vue';
+
+
+</script>
+
+<template>
+	<section class="checkOut">
+		<FormAccount />
+		<div class="checkOut-payment">
+			<h2 class="checkOut-payment__title">Payment Method</h2>
+			<p class="checkOut-payment__text">All transactions are secure and encrypted.</p>
+			<Payment />
+			<Button class='checkOut-payment_btn' :title='"Pay Now"' />
+		</div>
+	</section>
+</template>
+
+<style scoped lang='scss'>
+.checkOut {
+
+	&-payment {
+		margin-block-end: 100px;
+
+		&__title {
+			font-size: 22px;
+			font-weight: 600;
+		}
+
+		&_btn {
+			@include button(false, true, white);
+		}
+	}
+}
+</style>
