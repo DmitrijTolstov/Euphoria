d81e417d2c167ea6ba64da6d94dd30c9b56cf9cf
Tolstov Dmitrij <dima.tolstov.99@internet.ru>
2024-11-21 23:22:22 +0400

    putting logic into a separate component

---
+++ src/components/Cart/CartContainer.vue
@@ -0,0 +1,46 @@
+<script setup>
+import CartDiscount from '@/components/Cart/CartDiscount.vue';
+import Breadcrumbs from '@/components/Breadcrumbs.vue';
+import CartProduct from '@/components/Cart/CartProduct.vue'
+import EmptyCart from '@/components/Cart/EmptyCart.vue';
+import productCard from '@/stores/ProductCard';
+import { storeToRefs } from 'pinia';
+const store = productCard()
+const { wishlist } = storeToRefs(store)
+
+</script>
+<template>
+	<section class="cart">
+		<template v-if='wishlist'>
+			<div class="container">
+				<Breadcrumbs />
+				<div class="cart__text">
+					<p>Please fill in the fields below and click place order to complete your purchase!</p>
+					<p>Already registered? <a>Please login here</a></p>
+				</div>
+				<CartProduct />
+				<CartDiscount />
+			</div>
+		</template>
+		<template v-else>
+			<EmptyCart />
+		</template>
+	</section>
+</template>
+<style scoped lang='scss'>
+.cart {
+	overflow: hidden;
+
+	.breadCrumbs {
+		margin-block: 50px 30px;
+	}
+
+	&__text {
+		color: $cardText;
+
+		a {
+			color: $purple;
+		}
+	}
+}
+</style>
--- src/layouts/Cart.vue
+++ src/layouts/Cart.vue
@@ -1,8 +1,8 @@
 <script setup>
-import Footer from '@/components/Footer.vue';
-import Header from '@/components/Header.vue';
-import CartDiscount from '@/components/Cart/CartDiscount.vue';
-import CartProduct from '@/components/Cart/CartProduct.vue';
+import CartContainer from '../components/Cart/CartContainer.vue';
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+
 
 
 
@@ -10,34 +10,8 @@ import CartProduct from '@/components/Cart/CartProduct.vue';
 
 <template>
 	<Header />
-	<section class="cart">
-		<div class="container">
-			<div class="breadCrumbs"></div>
-			<div class="cart__text">
-				<p>Please fill in the fields below and click place order to complete your purchase!</p>
-				<p>Already registered? <a>Please login here</a></p>
-			</div>
-			<CartProduct />
-			<CartDiscount />
-		</div>
-	</section>
+	<CartContainer />
 	<Footer />
 </template>
 
-<style scoped lang='scss'>
-.cart {
-	overflow: hidden;
-
-	.breadCrumbs {
-		margin-block: 50px 30px;
-	}
-
-	&__text {
-		color: $cardText;
-
-		a {
-			color: $purple;
-		}
-	}
-}
-</style>
+<style scoped lang='scss'></style>
