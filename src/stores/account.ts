29d29508a7f4c85867edb54de5985bde73135afb
Tolstov Dmitrij <dima.tolstov.99@internet.ru>
2024-11-24 22:06:43 +0400

    storage for values related to the account page

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -3,17 +3,42 @@ import { defineStore } from 'pinia';
 export const account = defineStore('account',{
 	state: () => ({ 
 
-		component:'',
-		wishlist:[]
+		wishlist:[],
+		color:'',
+		size:'',
+		productCardInCart:[
+			{
+				id:1,
+				name:'Blue Flower Print Crop Top',
+				color:'red',
+				size:'L',
+				images:'src/assets/images/productCard/image-1.png',
+				price:23
+			},
+		]
 
 	 }),
   	getters: {
+		getProductCardInCart(state){
+			return state.productCardInCart.map(product => product)
+		},
+		sumPrice(state){
+			return state.productCardInCart.reduce((acc,card) => {
+				return acc + card.price
+			},0 )
+		}
 		
 		
   	},
   	actions: {
-		findComponent (component)  {
-			this.component = component
-}
+		deleteProduct(id:number){
+		console.log(id);
+			this.productCardInCart = this.productCardInCart.filter(product => product.id !== id)
+		},
+		addProduct(product){
+			console.log(product);
+			
+			this.productCardInCart.push({...product,color:this.color,size:this.size})
+		}
   },
 })
