4faffb6833ec700be17452b48dc8cc9e388f3dbb
Tolstov Dmitrij <dima.tolstov.99@internet.ru>
2024-11-24 22:04:42 +0400

    the condition for the output of a certain form of filling

--- src/components/Account/MyInfo.vue
+++ src/components/Account/MyInfo.vue
@@ -2,15 +2,31 @@
 
 import FormAccount from '@/components/Account/FormAccount.vue';
 import Address from '@/components/Account/Address.vue';
+import ContactDetailsForm from './ContactDetailsForm.vue';
+import AddAddressForm from './AddAddressForm.vue';
+
+let newAddress = false
 
 </script>
 <template>
 
 	<section class="info">
 		<h2 class="info__title">My Info</h2>
-		<p class="info__text">Contact Details</p>
-		<FormAccount :component='"contactDetails"' />
-		<Address />
+
+		<p v-if='newAddress' class="info__text">Add Address</p>
+
+		<p v-else class="info__text">Contact Details</p>
+
+		<template v-if='newAddress'>
+			<AddAddressForm />
+		</template>
+
+		<template v-else>
+			<ContactDetailsForm />
+			<Address @changeAddress='newAddress = true' />
+		</template>
+
+
 	</section>
 
 </template>
@@ -26,6 +42,7 @@ import Address from '@/components/Account/Address.vue';
 	&__text {
 		font-size: 22px;
 		font-weight: 600;
+		margin-block-end: 50px;
 	}
 
 
